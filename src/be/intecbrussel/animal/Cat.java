b72227b6da260f30de97fab7670089021cf25687
erikholl <erik.hollanders@student.intecbrussel.be>
2021-10-31 13:12:37 +0100

    Cat + Snake

---
+++ src/be/intecbrussel/animal/Cat.java
@@ -0,0 +1,25 @@
+package be.intecbrussel.animal;
+
+import javax.crypto.spec.PSource;
+
+public class Cat /* extends animal */ {
+
+//    public Cat() {
+//        this("Rachid");
+//    }
+//
+//    public Cat(String name) {
+//        super(name);
+//    }
+//
+//    @Override
+//    public void move() {
+//        println = "Catmove"
+//    }
+//
+//
+//    @Override
+//    public void makeNoise() {
+//         println = "Miaaauw";
+//    }
+}
--- src/be/intecbrussel/graphics/IsoScelesTriangle.java
+++ src/be/intecbrussel/graphics/IsoScelesTriangle.java
@@ -23,6 +23,7 @@ public class IsoScelesTriangle extends Triangle {
         super.setPerpendicular(getPerpendicular());
         super.setX(x);
         super.setY(y);
+        // setPerpendicular(perpendicular); // why does this not work? unclear
         setWidth(width);
     }
 
@@ -35,15 +36,18 @@ public class IsoScelesTriangle extends Triangle {
     @Override
     public void setWidth(int width) {
         super.setWidth(width);
+        //
     }
 
     @Override
     public void setPerpendicular(int perpendicular) {
         super.setPerpendicular(perpendicular);
     }
-    // we refer here back to the super method, so it does not make a lot of
-    // sense to override - we're doing it still though, to illustrate we get
-    // the hierarchy and how to refer back.
+    // we refer for setWidth and setPerpendicular back to the super
+    // method, so it does not make a lot of sense to override. we're doing
+    // it anyway, to illustrate we understand hierarchy and how to
+    // refer back. If we'd know the specific formula for width and
+    // perpendicular of IsoScelesTriangle, we'd included these instead.
 
     public static int getCount() {
         return count;
